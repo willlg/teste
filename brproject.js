5d0e827733f04d16f5cf00891ee98c76a25d6c93
willlg <williandealmeida1@gmail.com>
2025-07-15 15:57:45 -0300

    att url

---
+++ brproject.js
@@ -0,0 +1,81 @@
+﻿var Brproject = function() {
+	
+	this.url = "";
+	this.token = "";	
+	
+	this.gerarToken = function(data,callback_return) {
+		this.url = data.url;
+		
+		var chave = CryptoJS.MD5((CryptoJS.MD5(data.email).toString()+CryptoJS.MD5(data.senha).toString())).toString();
+		
+		var data_req = {
+			'chave' : chave,
+		};
+	
+		this.req('token/gerar','GET',data_req,callback_return);
+    };
+	
+	this.ehTokenValido = function(callback) {
+		var data = {
+			'token' : this.token,
+		};
+		return this.req('token/valido','GET',data,callback);
+	};
+	
+	this.getUltimaTarefa = function(callback) {
+		return this.req('tarefa/ultima','GET',null,callback);
+	};
+	this.test = function(){
+		alert("teste");
+	};
+	this.req = function(url,type,data,callback){
+		var headers
+		
+		if(this.token != null){
+			headers = {
+				'Authorization':this.token,
+			};
+		}
+		else{
+			headers = null;
+		}
+		
+		$.ajax({
+			url: this.url+'/api/'+url,
+			type:type,
+			headers: headers,
+			dataType: 'json',
+			crossDomain:true,
+			data: data,
+			beforeSend:function() {
+				if(callback.beforeSend != null)
+					callback.beforeSend();
+			},
+			success:function(data) {
+				if(callback.success != null)
+					callback.success(data);
+			},			
+			error:function (xhr, ajaxOptions, thrownError){
+				if(callback.error != null)
+					callback.error(xhr, ajaxOptions, thrownError);
+			},
+			complete: function(data){
+				if(callback.complete != null)
+					callback.complete(data);
+			}
+		});
+	};
+	
+	this.iniciarTarefa = function(tarefa,callback) {
+		var data = {
+			tarefa: tarefa
+		};
+		return this.req('tarefa/iniciar','POST',data,callback);
+	};
+	
+	this.pararTarefa = function(callback) {
+		return this.req('tarefa/parar','POST',null,callback);
+	};
+	
+	
+};
---
+++ client.js
@@ -0,0 +1,25 @@
+//var t = window.TrelloPowerUp.iframe();
+console.log('client.js carregado') 
+
+window.TrelloPowerUp.initialize({
+  'card-buttons': function (t, options) {
+    return [{
+      icon: window.BRPROJECT_BASE_URL + '/images/project.png',
+      text: 'BRProject',
+      callback: function (t) {
+        return t.popup({
+          title: 'BRProject',
+          url: window.BRPROJECT_BASE_URL + '/popup.html',
+          height: 400
+        });
+      }
+    }];
+  },
+  'show-settings': function (t, options) {
+    return t.popup({
+      title: 'Configurações BRProject',
+      url: window.BRPROJECT_BASE_URL + '/popup.html',
+      height: 400
+    });
+  }
+});
