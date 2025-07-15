2ac84a907f76ccba191f21748915e6d821ef8736
willlg <williandealmeida1@gmail.com>
2025-07-15 16:17:16 -0300

    att teste

--- client.js
+++ client.js
@@ -1,8 +1,10 @@
 //var t = window.TrelloPowerUp.iframe();
+window.BRPROJECT_BASE_URL = 'https://brproject.vercel.app';
 console.log('client.js carregado') 
 
 window.TrelloPowerUp.initialize({
   'card-buttons': function (t, options) {
+    console.log('card-buttons carregado');
     return [{
       icon: window.BRPROJECT_BASE_URL + '/images/project.png',
       text: 'BRProject',
